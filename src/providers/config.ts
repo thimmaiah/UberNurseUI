2d2482bb63aa8cc4374faf9b0ab7d001a95d3700
thimmaiah <thimmaiah@gmail.com>
2017-07-17 18:31:11 +0530

    REmoved deeplinks and cleaned up package.json

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the PaymentApi provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class Config {
 
