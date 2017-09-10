901ed274f7a409bd49ab17bfa0b315f0a19bec3d
thimmaiah <thimmaiah@gmail.com>
2017-09-10 09:23:18 +0530

    Added production code-push keys

---
+++ release.sh
@@ -0,0 +1,7 @@
+# This generates the production apk
+rm Connuct.apk
+ionic cordova build --release android
+# password is connuct
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore Connuct.keystore /home/thimmaiah/work/angular/UberNurseUI/platforms/android/build/outputs/apk/android-release-unsigned.apk Connuct
+zipalign -v 4 /home/thimmaiah/work/angular/UberNurseUI/platforms/android/build/outputs/apk/android-release-unsigned.apk Connuct.apk
+apksigner verify Connuct.apk 
