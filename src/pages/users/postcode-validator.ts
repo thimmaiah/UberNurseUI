6921f8490d2b475628f402c6b5741d40ba72096e
thimmaiah <thimmaiah@gmail.com>
2017-09-26 09:21:32 +0530

    Changed postcode validation to 5 digit

--- src/pages/users/postcode-validator.ts
+++ src/pages/users/postcode-validator.ts
@@ -18,7 +18,7 @@ export class PostCodeValidator {
       console.log("PostCodeValidator: checkPostCode", control.value);
       //control.markAsPending();
 
-      if (control.value && control.value.length >= 6) {
+      if (control.value && control.value.length >= 5) {
         PostCodeValidator.postCodeApi.getPostCodes(control.value).subscribe(
           resp => {
             console.log(resp);
