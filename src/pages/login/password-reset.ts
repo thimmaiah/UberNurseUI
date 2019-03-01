efd16cf78b0b15f9d6d511fc2f5c3c0821265a00
Mohith Thimmaiah <thimmaiah@gmail.com>
2019-03-01 19:16:36 +0530

    Fixed passwd reset

--- src/pages/login/password-reset.ts
+++ src/pages/login/password-reset.ts
@@ -43,58 +43,37 @@ export class PasswordReset {
 
   reset() {
 
-    let loader = this.loadingController.create({
-      content: 'Reset in progress ...'
-    });
+    if(this.password != this.confirm_password) {
+      this.respUtility.showWarning("Passwords dont match, please re-enter the password and confirm.")
+    } else {
 
-    loader.present();
-    this.http.get(`auth/password/edit?reset_password_token=${this.token}&redirect_url=auth/password`).subscribe(
-      res => {
-        console.log(res);
-        this.headerInfo = res.json();
-        let params = {password: this.password, password_confirmation: this.confirm_password}
-        loader.dismiss();this.resetPassword(this.headerInfo, params);
-        
-      },
-      error => {
-        console.log(error);
-        loader.dismiss();
-        this.respUtility.showFailure(error);
-      }
-    );
+      let loader = this.loadingController.create({
+        content: 'Reset in progress ...'
+      });
+
+      loader.present();
+      this.http.post(`${this.config.props["API_URL"]}/users/reset_password.json`, {token: this.token, password: this.password}).subscribe(
+        res => {
+          console.log(res);
+          this.headerInfo = res.json();
+          loader.dismiss();
+          console.log(this.headerInfo);
+          if(this.headerInfo["reset"] == true) {
+            this.respUtility.showSuccess("Password reset successfully, please login with the new password.")
+            this.navCtrl.pop();
+          } else {
+            this.respUtility.showWarning("Password reset failed, please contact support.")
+          }
+          
+        },
+        error => {
+          console.log(error);
+          loader.dismiss();
+          this.respUtility.showFailure(error);
+        }
+      );
+
+    }
   }
-
-  resetPassword(headerInfo: any, params: any) {
-
-    console.log(headerInfo);
-
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json;q=0.9,*/*;q=0.8'); // Fix for Firefox
-    headers.append('access-token', headerInfo['token']);
-    headers.append('client', headerInfo['client_id']);
-    headers.append('expiry', headerInfo['expiry']);
-    headers.append('uid', headerInfo['uid']);
-    headers.append('token-type', 'Bearer');
-
-    console.log(params);
-    let h = new RequestOptions({
-      headers: headers
-    });
-    
-    
-    this.http.put(`${this.config.props['API_URL']}/auth/password`, params, h).subscribe(
-      res => {
-        console.log(res);
-        this.respUtility.showSuccess("Password reset successfully. Please login");
-        this.navCtrl.popToRoot();
-      },
-      error => {
-        console.log(error);
-        this.respUtility.showFailure(error);
-      }
-    );
-  }
-
 
 }
